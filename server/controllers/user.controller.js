e9dd594dcb5b084420d726853399e1328bf57f43
Stephen <>
2022-08-23 21:12:24 -0700

    Created auth api endpoints

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model.js';
 import extended from 'lodash/extend.js';
-import errorHandler from './error.controller.js';
+import errorHandler from './../helpers/dbErrorHandler.js';
 
 
 
--- server/express.js
+++ server/express.js
@@ -4,10 +4,12 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import helmet from 'helmet';
 import userRoutes from './routes/user.routes.js';
+import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 
-app.use('/',userRoutes)
+app.use('/', authRoutes);
+app.use('/',userRoutes);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
